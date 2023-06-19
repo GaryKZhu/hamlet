ec7621327c7499929607e3e8ed9c8a4b84240e4a
Gary Zhu <garyzhu@Garys-MacBook-Air.local>
2023-06-19 18:57:48 -0400

    new changes

---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject card1;
+    public GameObject card2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
