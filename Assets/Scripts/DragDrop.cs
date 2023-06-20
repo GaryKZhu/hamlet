04a6025765075099a9dc4478269515f6474462ca
Gary Zhu <garyzhu@Garys-MacBook-Air.local>
2023-06-19 21:29:30 -0400

    ui draw cards

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -4,22 +4,57 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
+    public GameObject Canvas;
+    public GameObject DropZone;
+
     private bool isDragging = false;
+    private GameObject startParent;
+    private Vector2 startPosition;
+    private GameObject dropZone;
+    private bool isOverDropZone; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("Drop Zone"); 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colliding!");
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Uncolliding!");
+        isOverDropZone = false;
+        dropZone = null;
+
     }
 
     public void startDrag()
     {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
     }
 
     public void EndDrag()
     {
         isDragging = false;
+        if(isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false); 
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false); 
+        }
     }
 
     // Update is called once per frame
@@ -28,6 +63,7 @@ public class DragDrop : MonoBehaviour
         if(isDragging)
         {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
         }
     }
 }
--- Assets/Scripts/DrawCards.cs
+++ Assets/Scripts/DrawCards.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class DrawCards : MonoBehaviour
 {
-    public GameObject card1;
-    public GameObject card2;
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea; 
+    public void onClick()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject card = Instantiate(Card1, new Vector2(0, 0), Quaternion.identity);
+            card.transform.SetParent(PlayerArea.transform, false);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject card = Instantiate(Card1, new Vector2(0, 0), Quaternion.identity);
+            card.transform.SetParent(EnemyArea.transform, false);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
